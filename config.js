a2f8c1050d4b51f8f812d9fb7cfa0e1c03d443e3
THUNDERluzer <95018914+THUNDERluzer@users.noreply.github.com>
2024-09-03 10:11:10 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,10 +5,10 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "put sesson id",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/357b75726739a8b9c68de.jpg",
-SUDO_NB: process.env.SUDO_NB || "ඔයාගෙ owner number එක දෙන්න",
+SESSION_ID: process.env.SESSION_ID || "3bRX0YSK#XF9FTKpwoZlY4_VrPfusJjq2YHYhfpnTdrw9Il7dIAE",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/7721cf222f9790f3fcd1b.jpg",
+SUDO_NB: process.env.SUDO_NB || "94782543893",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
-MODE: process.env.MODE || "public",
+MODE: process.env.MODE || "private",
 AUTO_VOICE: "true"
 };
